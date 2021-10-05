b891b4ea3d5c4687169abf55029ca8c761bbb646
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-10-05 08:09:45 -0400

    create global css file + dark mode

--- countries-web-app/components/header.vue
+++ countries-web-app/components/header.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="header">
     <h2>Where in the world?</h2>
+    <div class="header-switch">
+      <p>Dark Mode</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+   
+  padding-left: 5%;
+  padding-right: 5%;
+
+  height: 100px;
+}
+
+</style>
