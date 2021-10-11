675d5554b0ca30bdc2e2463890c425a4840280d2
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-10-11 16:04:22 -0400

    Update index.vue

--- countries-web-app/pages/index.vue
+++ countries-web-app/pages/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <Header/>
+  <div class="index w-100 d-flex flex-row justify-content-around flex-wrap">
+    <div v-for="country in countries" :key="country.name">
+      <Country
+        :countryName="country.name.common"
+        :countryFlag="country.flags.svg"
+        :countryPopulation="country.population"
+        :countryRegion="country.region"
+        :countryCapital="country.capital"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Header from '@/components/header.vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({
+  async asyncData({ $axios }) {
+    const apiURL = "https://restcountries.com/v3.1/all";
+    const countries = await $axios.$get(apiURL);
+    return { countries };
+  },
+});
 </script>
