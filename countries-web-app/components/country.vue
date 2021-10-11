5b2b4631d17acf6855bb62933581e4fd16de14c6
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-10-11 16:04:18 -0400

    Create country.vue

---
+++ countries-web-app/components/country.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-card
+      :title="countryName"
+      :img-src="countryFlag"
+      img-height="175"
+      img-width="300"
+      style="width: 350px"
+      class="mb-2"
+    >
+      <b-card-text> <b>Population:</b> {{ countryPopulation }} </b-card-text>
+
+      <b-card-text> <b>Region:</b> {{ countryRegion }} </b-card-text>
+
+      <b-card-text>
+       
+        <div v-for="capital in countryCapital" :key="capital">
+         <b>Capital:</b>  {{ capital }}
+        </div></b-card-text
+      >
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    countryName: String,
+    countryFlag: String,
+    countryPopulation: Number,
+    countryRegion: String,
+    countryCapital: Array,
+  },
+});
+</script>
